6fa4e0425db83c7964e13e6228b224f831f74931
松本雄揮 <yukiji.0307@gmail.com>
2020-08-20 14:36:00 +0900

    hello component create

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Hello } from './Hello'
 
 export const App = () => {
   return (
     <div className="App">
       <p>Hello World!!</p>
+      <Hello />
     </div>
   )
 }
